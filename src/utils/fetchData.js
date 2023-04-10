f8c1f3b5ea0a75b27fe931c816db94e567ed3add
seemo6.9 <alslamat407@gmail.com>
2023-04-10 09:50:27 +0300

    this is Gym website to pract on API's with React.js

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '85193bb92cmsh8c7a27f0a0e2ff3p1d7bdcjsn94b0d22a9482',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
